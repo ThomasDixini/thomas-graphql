5b0bc50a094e10d5f4d87654d3518f4a954fd3e9
Thomás Dixini <thomasdixini@gmail.com>
2023-09-18 21:41:48 -0300

    Criando Resolver de Courses

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,24 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/thomasdixini/thomas-graphql/graph"
+	"github.com/thomasdixini/thomas-graphql/internal/database"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+	courseDb := database.NewCourse(db)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDb,
+		CourseDB:   courseDb,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
